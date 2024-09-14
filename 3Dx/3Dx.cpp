5dd3f6e1c653bd63343c80c400735c16fbecedbe
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-15 01:03:50 +0300

    comment directX definitions

--- 3Dx/3Dx.cpp
+++ 3Dx/3Dx.cpp
@@ -15,36 +15,36 @@
 #pragma comment (lib, "d3dx10.lib")
 #pragma comment (lib, "DirectXTK.lib")
 
-// define the screen resolution
-#define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT 600
+//// define the screen resolution
+//#define SCREEN_WIDTH  800
+//#define SCREEN_HEIGHT 600
 
-// Namespace
-using namespace DirectX;
-
-// global declarations
-IDXGISwapChain *swapchain;             // the pointer to the swap chain interface
-ID3D11Device *dev;                     // the pointer to our Direct3D device interface
-ID3D11DeviceContext *devcon;           // the pointer to our Direct3D device context
-ID3D11RenderTargetView *backbuffer;    // the pointer to our back buffer
-ID3D11InputLayout *pLayout;            // the pointer to the input layout
-ID3D11VertexShader *pVS;               // the pointer to the vertex shader
-ID3D11PixelShader *pPS;                // the pointer to the pixel shader
-ID3D11Buffer *pVBuffer;                // the pointer to the vertex buffer
-
-// a struct to define a single vertex
-struct VERTEX{FLOAT X, Y, Z; D3DXCOLOR Color;};
-
-
-// function prototypes
-void InitD3D(HWND hWnd);    // sets up and initializes Direct3D
-void RenderFrame(void);     // renders a single frame
-void CleanD3D(void);        // closes Direct3D and releases memory
-void InitGraphics(void);    // creates the shape to render
-void InitPipeline(void);    // loads and prepares the shaders
-
-void SetupWorldMatrix(float rotationAngle, XMMATRIX &worldMatrix);
-void Render(ID3D11DeviceContext *deviceContext, ID3D11Buffer *constantBuffer, float rotationAngle);
+//// Namespace
+//using namespace DirectX;
+//
+//// global declarations
+//IDXGISwapChain *swapchain;             // the pointer to the swap chain interface
+//ID3D11Device *dev;                     // the pointer to our Direct3D device interface
+//ID3D11DeviceContext *devcon;           // the pointer to our Direct3D device context
+//ID3D11RenderTargetView *backbuffer;    // the pointer to our back buffer
+//ID3D11InputLayout *pLayout;            // the pointer to the input layout
+//ID3D11VertexShader *pVS;               // the pointer to the vertex shader
+//ID3D11PixelShader *pPS;                // the pointer to the pixel shader
+//ID3D11Buffer *pVBuffer;                // the pointer to the vertex buffer
+//
+//// a struct to define a single vertex
+//struct VERTEX{FLOAT X, Y, Z; D3DXCOLOR Color;};
+//
+//
+//// function prototypes
+//void InitD3D(HWND hWnd);    // sets up and initializes Direct3D
+//void RenderFrame(void);     // renders a single frame
+//void CleanD3D(void);        // closes Direct3D and releases memory
+//void InitGraphics(void);    // creates the shape to render
+//void InitPipeline(void);    // loads and prepares the shaders
+//
+//void SetupWorldMatrix(float rotationAngle, XMMATRIX &worldMatrix);
+//void Render(ID3D11DeviceContext *deviceContext, ID3D11Buffer *constantBuffer, float rotationAngle);
 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
