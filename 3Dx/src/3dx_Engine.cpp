9dfe2cc84ad5f40797bddc5c942b39edaa88ae6e
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-15 00:59:57 +0300

    add engine update() test vetsion

--- 3Dx/src/3dx_Engine.cpp
+++ 3Dx/src/3dx_Engine.cpp
@@ -9,3 +9,37 @@ bool DxEngine::processMessages()
 {
 	return this->renderWindow.processMessages();
 }
+
+void DxEngine::update()
+{
+	while (!keyboard.charBufferIsEmpty())
+	{
+		UCH uch = keyboard.readChar();
+
+		// For console out
+		std::string out_message = "Char: ";
+		out_message += uch;
+		out_message += "\n";
+		OutputDebugStringA(out_message.c_str());
+	}
+
+	while (!keyboard.keyBufferIsEmpty())
+	{
+		KeyboardEvent keybEvent = keyboard.readKey();
+		UCH keyCode = keybEvent.getKeyCode();
+		
+		// For console out
+		std::string out_message = "";
+		if(keybEvent.isPress())
+		{
+			out_message += "Key press: ";
+		}
+		if(keybEvent.isRelease())
+		{
+			out_message += "Key release: ";
+		}
+		out_message += keyCode;
+		out_message += "\n";
+		OutputDebugStringA(out_message.c_str());
+	}
+}
