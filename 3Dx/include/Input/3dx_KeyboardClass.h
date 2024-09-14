2ca211f9215c7f5019deff686d71674ecdd2436e
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-15 00:59:03 +0300

    Create 3dx_KeyboardClass.h

---
+++ 3Dx/include/Input/3dx_KeyboardClass.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "3dx_KeyboardEvent.h"
+#include <queue>
+
+// InputKeyboardClass - a class that handles keyboard keystrokes
+class InputKeyboardClass
+{
+public:
+	InputKeyboardClass();
+
+	bool keyIsPressed(const UCH keycode);
+	bool keyBufferIsEmpty();
+	bool charBufferIsEmpty();
+
+	KeyboardEvent readKey();
+	UCH readChar();
+
+	void onKeyPressed(const UCH key);
+	void onKeyReleased(const UCH key);
+	void onChar(const UCH key);
+
+	void enableAutoRepeatKeys();
+	void disableAutoRepeatKeys();
+	void enableAutoRepeatChars();
+	void disableAutoRepeatChars();
+
+	bool isKeysAutoRepeat();
+	bool isCharsAutoRepeat();
+
+private: 
+	bool m_autoRepeatKeys = false;
+	bool m_autoRepeatChars = false;
+	bool m_keyStates[256];
+
+	std::queue<KeyboardEvent> m_keyBuffer;
+	std::queue<UCH> m_charBuffer;
+};
