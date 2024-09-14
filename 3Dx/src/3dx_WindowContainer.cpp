e0d9c1fc73761f34aea0f4ed838c6d3ca1ba6393
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-15 01:00:19 +0300

    add msg switch

--- 3Dx/src/3dx_WindowContainer.cpp
+++ 3Dx/src/3dx_WindowContainer.cpp
@@ -2,5 +2,45 @@
 
 LRESULT WindowContainer::WindowProc(HWND hWnd, UINT message, WPARAM param, LPARAM lParam)
 {
-	return DefWindowProc(hWnd, message, param, lParam);
+	switch (message)
+	{
+		case WM_KEYDOWN:
+		{
+			UCH keyCode = static_cast<UCH>(param);
+			if(keyboard.isKeysAutoRepeat())
+			{
+				keyboard.onKeyPressed(keyCode);
+			}
+			else
+			{
+				const bool wasPressed = lParam & 0x40000000;
+				if (!wasPressed)
+					keyboard.onKeyPressed(keyCode);
+			}
+			return 0;
+		}
+		case WM_KEYUP:
+		{
+			UCH keyCode = static_cast<UCH>(param);
+			keyboard.onKeyReleased(keyCode);
+			return 0;
+		}
+		case WM_CHAR:
+		{
+			UCH uch = static_cast<UCH>(param);
+			if(keyboard.isCharsAutoRepeat())
+			{
+				keyboard.onChar(uch);
+			}
+			else
+			{
+				const bool wasPressed = lParam & 0x40000000;
+				if (!wasPressed)
+					keyboard.onChar(uch);
+			}
+			return 0;
+		}
+		default:
+			return DefWindowProc(hWnd, message, param, lParam);
+	}
 }
