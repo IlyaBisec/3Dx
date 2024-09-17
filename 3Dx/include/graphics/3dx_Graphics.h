fb846f91977528da75254b7e05128d5efffd0f69
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-18 00:41:15 +0300

    add init shadets

--- 3Dx/include/graphics/3dx_Graphics.h
+++ 3Dx/include/graphics/3dx_Graphics.h
@@ -15,11 +15,15 @@ public:
 
 private:
 	bool initializeDirectX(HWND hWnd, int width, int height);
+	bool initializeShaders();
 
 	MWRL::ComPtr<ID3D11Device> m_device;
 	MWRL::ComPtr<ID3D11DeviceContext> m_deviceContext;
 	MWRL::ComPtr<IDXGISwapChain> m_swapChain;
 	MWRL::ComPtr<ID3D11RenderTargetView> m_renderTargetView;
+
+	MWRL::ComPtr<ID3D11InputLayout> m_inputLayout;
+	MWRL::ComPtr<ID3D10Blob> m_vertexBuffer;
 };
 
 #endif // !_3DX_GRAPHICS_H_
