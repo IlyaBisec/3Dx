4670fd18e8fabc4e624f1265e83917718827df75
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-18 00:40:35 +0300

    add wstring log

--- 3Dx/include/3dx_ErrLogger.h
+++ 3Dx/include/3dx_ErrLogger.h
@@ -7,8 +7,8 @@ class ErrorLogger
 {
 public:
 	static void log(const std::string &message);
-	// Wstring error log
 	static void log(HRESULT hResult, const std::string message);
+	static void log(HRESULT hResult, std::wstring message);
 
 private:
 };
