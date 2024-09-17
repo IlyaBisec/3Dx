eba2edef363adb3408b886814a746c0d5026a37b
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-17 18:39:35 +0300

    add MouseClass and DxGraphics

--- 3Dx/include/3dx_WindowContainer.h
+++ 3Dx/include/3dx_WindowContainer.h
@@ -1,15 +1,19 @@
 #pragma once
 #include "3dx_RenderWindow.h"
 #include "Input/3dx_KeyboardClass.h"
+#include "Input/3dx_MouseClass.h"
+#include "graphics/3dx_Graphics.h"
 
 class WindowContainer
 {
 public:
+	WindowContainer();
 	LRESULT WindowProc(HWND hWnd, UINT message, WPARAM param, LPARAM lParam);
 
 protected:
 	RenderWindow renderWindow;
+	DxGraphics graphics;
 	InputKeyboardClass keyboard;
-
+	InputMouseClass mouse;
 private:
 };
