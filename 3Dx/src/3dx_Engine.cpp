2d2ef114ac7749e5a3af5e18382ca428e4c8cc31
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-17 18:41:27 +0300

    add mouse input and graphic init with help dx

--- 3Dx/src/3dx_Engine.cpp
+++ 3Dx/src/3dx_Engine.cpp
@@ -2,7 +2,15 @@
 
 bool DxEngine::initialize(HINSTANCE hInstance, std::string windowTitle, std::string windowClass, int width, int height)
 {
-	return this->renderWindow.initialize(hInstance, windowTitle, windowTitle, width, height);
+	if (!this->renderWindow.initialize(this, hInstance, windowTitle, windowClass, width, height))
+		return false;
+
+	if (!graphics.initialize(this->renderWindow.getHWND(), width, height))
+		return false;
+
+	return true;
+
+	///return this->renderWindow.initialize(hInstance, windowTitle, windowTitle, width, height);
 }
 
 bool DxEngine::processMessages()
@@ -17,19 +25,19 @@ void DxEngine::update()
 		UCH uch = keyboard.readChar();
 
 		// For console out
-		std::string out_message = "Char: ";
+		/*std::string out_message = "Char: ";
 		out_message += uch;
 		out_message += "\n";
-		OutputDebugStringA(out_message.c_str());
+		OutputDebugStringA(out_message.c_str());*/
 	}
 
 	while (!keyboard.keyBufferIsEmpty())
 	{
-		KeyboardEvent keybEvent = keyboard.readKey();
+		DxKeyboardEvent keybEvent = keyboard.readKey();
 		UCH keyCode = keybEvent.getKeyCode();
 		
 		// For console out
-		std::string out_message = "";
+		/*std::string out_message = "";
 		if(keybEvent.isPress())
 		{
 			out_message += "Key press: ";
@@ -40,6 +48,22 @@ void DxEngine::update()
 		}
 		out_message += keyCode;
 		out_message += "\n";
-		OutputDebugStringA(out_message.c_str());
+		OutputDebugStringA(out_message.c_str());*/
+	}
+
+	while (!mouse.eventByfferIsEmpty())
+	{
+		DxMouseEvent mevent = mouse.readEvent();
+		// For console out
+		/*if(mevent.getType() == DxMouseEvent::EMEventType::tRawMove)
+		{
+			std::string msg = "X: ";
+			msg += std::to_string(mevent.getPositionX());
+			msg += ", ";
+			msg += "X: ";
+			msg += std::to_string(mevent.getPositionY());
+			msg += "\n";
+			OutputDebugStringA(msg.c_str());
+		}*/
 	}
 }
