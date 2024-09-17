d89030399fbad78f971d07ea2a0ae7bcc45ccb7a
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-18 00:41:01 +0300

    delete starter pack defines

--- 3Dx/Resource.h
+++ 3Dx/Resource.h
@@ -1,30 +1,17 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Включаемый файл, созданный в Microsoft Visual C++.
-// Используется 3Dx.rc
+// Used by 3dx Vs2022 3Dx.rc 
+// ilya_bisec 2024 autumn
 
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_3DX_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_3DX_EDITOR			107
-#define IDI_SMALL				108
-#define IDC_3DX_EDITOR			109
-#define IDC_3DX_ICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
 // Следующие стандартные значения для новых объектов
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 
 #define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NEXT_RESOURCE_VALUE	101
+#define _APS_NEXT_COMMAND_VALUE		40001
+#define _APS_NEXT_CONTROL_VALUE		1001
+#define _APS_NEXT_SYMED_VALUE		101
 #endif
 #endif
