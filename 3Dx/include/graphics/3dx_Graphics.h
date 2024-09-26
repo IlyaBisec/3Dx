e8f1cf567f253b9848848c61279f274be148536e
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-09-26 22:47:02 +0300

    add depth, rasterizerstate and font declarations

--- 3Dx/include/graphics/3dx_Graphics.h
+++ 3Dx/include/graphics/3dx_Graphics.h
@@ -6,6 +6,9 @@
 #include "3dx_Shaders.h"
 #include "3dx_Vertex.h"
 
+#include <SpriteBatch.h>
+#include <SpriteFont.h>
+
 namespace MWRL =  Microsoft::WRL;
 
 // DxGraphics - class which initialize 3dx graphics
@@ -26,9 +29,19 @@ private:
 	MWRL::ComPtr<ID3D11RenderTargetView> m_renderTargetView;
 
 	MWRL::ComPtr<ID3D11Buffer> m_vertexBuffer;
+	MWRL::ComPtr<ID3D11Buffer> m_vertexBuffer2;
 
 	VertexShader m_vertexShader;
 	PixelShader  m_pixelShader;
+
+	MWRL::ComPtr<ID3D11DepthStencilView> m_depthStencilView;
+	MWRL::ComPtr<ID3D11Texture2D> m_depthStencilBuffer;
+	MWRL::ComPtr<ID3D11DepthStencilState> m_depthStencilState;
+
+	MWRL::ComPtr<ID3D11RasterizerState> m_rasterizerState;
+
+	std::unique_ptr<DirectX::SpriteBatch> m_spriteBatch;
+	std::unique_ptr<DirectX::SpriteFont> m_spriteFont;
 };
 
 #endif // !_3DX_GRAPHICS_H_
